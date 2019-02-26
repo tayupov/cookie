4cf1a805383476b7e025d2398b45b90516ce0d1d
tayupov <romantajupow@gmail.com>
2019-02-26 18:54:08 +0100

    Updates

---
+++ precache-manifest.c1d7a7b4cd1471c01c11bc5bc9f5ecb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cookie/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c06727a1efca435d7b2a",
+    "url": "/cookie/static/js/runtime~main.c06727a1.js"
+  },
+  {
+    "revision": "403fb31ca537efaa7bbe",
+    "url": "/cookie/static/js/main.403fb31c.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/cookie/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "403fb31ca537efaa7bbe",
+    "url": "/cookie/static/css/main.b562601e.chunk.css"
+  },
+  {
+    "revision": "992ca8728048a743a6caefa08b1087b0",
+    "url": "/cookie/index.html"
+  }
+];
